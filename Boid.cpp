60a10e92aba2629606dcf8be3c8c89820a888949
egarnier <e.garnier30@gmail.com>
2014-12-15 22:14:44 +0000

    Added printf to find the error during the prog execution

--- Boid.cpp
+++ Boid.cpp
@@ -38,6 +38,7 @@ Boid::Boid(void)
 {
 	pop = NULL;
 	N = 0;
+	printf("youhou je suis dans le constructor by default !\n");
 }
 
 Boid::Boid(Agent firstAgent)
@@ -83,25 +84,30 @@ void Boid::AddAgent(Agent newAgent)
 {
 	if(pop == NULL)
 	{
+		printf("youhou je suis dans le AddAgent() dans le cas ou pop est nul !\n");
 		N = 1;
 		pop = new Agent[1];
 		pop[0] = newAgent;
+		printf("youhou je suis dans le AddAgent() dans le cas ou pop est nul !\n");
 	}
 	else
 	{
+		printf("youhou je suis dans le AddAgent() dans le cas ou pop est non nul !\n");
 		Agent* tmp = new Agent[N];
 		for(int i = 0 ; i < N ; i++)
 		{
 			tmp[i] = pop[i];
 		}
-		N += 1;
+		N = N + 1;
 		pop = new Agent[N];
 		for(int j = 0 ; j < N-1 ; j++)
 		{
 			pop[j] = tmp[j];
 		}
 		pop[N-1] = newAgent;
-		delete tmp;
+		printf("youhou je suis dans le AddAgent() dans le cas ou pop est nul !\n");
+		delete[] tmp;
+		printf("youhou je suis dans le AddAgent() dans le cas ou pop nonest nul !\n");
 	}
 }
 
